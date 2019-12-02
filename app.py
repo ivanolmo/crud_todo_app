b9d81fb92ecd4e3c0e7c09015a4c7da939f80729
Ivan Olmo <ivan.olmo@gmail.com>
2019-12-01 20:14:06 -0600

    Add functionality to check items as completed and/or delete items

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect,\
+    url_for, abort
 from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
 from flask_migrate import Migrate
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://' \
                                         'postgres:asdf@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -21,24 +23,64 @@ class Todo(db.Model):
     def __repr__(self):
         return f'<ToDo {self.id} - {self.item}>'
 
+
 # Migrate will now create tables
 # db.create_all()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 
 @app.route('/todo/create', methods=['POST'])
 def create_todo():
-    new_item = request.get_json()['todo-item']
-    todo = Todo(item=new_item)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify({
-        'todo-item': todo.item
-    })
+    error = False
+    body = {}
+    try:
+        new_item = request.get_json()['todo-item']
+        todo = Todo(item=new_item, completed=False)
+        db.session.add(todo)
+        db.session.commit()
+        body['id'] = todo.id
+        body['item'] = todo.item
+        body['completed'] = todo.completed
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
+
+
+@app.route('/todo/<todo_id>/set-completed', methods=['POST'])
+def check_completed(todo_id):
+    try:
+        new_completion = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = new_completion
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+
+@app.route('/todo/<todo_id>/delete-todo', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({'success': True})
 
 
 if __name__ == '__main__':
